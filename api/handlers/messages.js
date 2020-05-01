f707815077046164714b32ec43882fed33f26478
Kanav Petkar <k.petkar21@gmail.comm>
2020-05-01 16:39:28 -0400

    Resolving merge conflicts and added api endpoints
    wip
    
    added getAllInfluencers
    
    wip

--- api/handlers/messages.js
+++ api/handlers/messages.js
@@ -1,28 +1,64 @@
+var dotenv = require('dotenv').config();
+var aws = require('aws-sdk')
 const db = require('../db_connect');
 
 
+function sendSESMessage(body, title, receiverEmail, receiverName, requestorName, tag1, tag2) {
+  var templateData = {
+    name : receiverName,
+    requestor_name : requestorName,
+    title : title,
+    body : body,
+    tag1 : tag1,
+    tag2 : tag2
+  }
 
-  //POST endpoint to create a message
-  module.exports.createMessage = (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
+   const SESConfig = {
+    apiVersion: '2010-12-01',
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SES_SECRET_ACCESS_KEY,
+    region: process.env.AWS_SES_REGION
+  };
 
-    const reqBody = JSON.parse(event.body);
-    const firstName = reqBody.firstName;
-    const lastName = reqBody.lastName;
-    const emailAddress = reqBody.emailAddress;
-  
-    db.query('INSERT INTO users(first_name, last_name, e_mail) VALUES($1, $2, $3) RETURNING user_id', [firstName, lastName, emailAddress])
+  var params = {
+    Source: 'noreply@quire.gq',
+    Destination: {
+      ToAddresses: [
+       receiverEmail 
+      ]
+    },
+    Template: 'MARTO_3',
+    TemplateData: JSON.stringify(templateData)
+  };
+
+  new aws.SES(SESConfig).sendTemplatedEmail(params).promise()
+  .catch(err =>{
+    throw err;
+  })
+};
+module.exports.sendSESMessage = sendSESMessage;
+
+module.exports.handleIncomingMail = (event, callback, context) =>{
+  console.log(event) 
+}
+//GET endpoint to get all contracts for a given user (not influencer)
+module.exports.getMessagesForUser = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  var userID = event.path.id
+  db.query('SELECT * FROM messages WHERE sender_id=$1 or receiver_id=$1 ', [userID])
       .then(res => {
-        callback(null, {
-          statusCode: 200,
-          body: 'successfully inserted user'
-        })
+          callback(null, {
+              statusCode: 200,
+              body: JSON.stringify(res.rows)
+          })
       })
       .catch(e => {
-        console.log(e);
-        callback(null, {
-          statusCode: e.statusCode || 500,
-          body: 'Error:' + e
-        });
-      }); 
-  };
+          console.log(e);
+          callback(null, {
+              statusCode: e.statusCode || 500,
+              body: 'Error:' + e
+          });
+      })
+
+};
