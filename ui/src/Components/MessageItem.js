d65e05287211d9bb1b28c3010a0371d3b3d19780
Kanav Petkar <k.petkar21@gmail.comm>
2020-05-18 18:38:28 -0400

    wip

--- ui/src/Components/MessageItem.js
+++ ui/src/Components/MessageItem.js
@@ -4,24 +4,26 @@ const MessageItem = (props) => {
   const [active, setActive] = React.useState(1);
 
   //standardizing the message preview, otherwise the messages can be of variable length and mess up the image sizing
-  const messagePreview = (message) => {
+/*   const messagePreview = (message) => {
     if (message.length < 63){
       var i = message.length;
       while (i < 63){
-        message=message + "\u00A0";
+        message=message + " ";
+        i++;
       }
+      console.log(message)
       return message;
     }else{
       return (message.substring(0,62) + '...')
     }
-  }
+  } */
 
   return (
     <>
       <div className="sidebar">
         <ul className="sideList">
-          {props.data &&
-            props.data.map((item) => (
+          {props.messageData &&
+            props.messageData.map((item) => (
               <li className={active && active === item.message_id ? "active":""} style={{cursor:'pointer'}} onClick={()=>{setActive(item.message_id)}}>
                 <div className="imgHolder">
                   <img src="https://source.unsplash.com/random"></img>
@@ -29,7 +31,7 @@ const MessageItem = (props) => {
                 <div className="txt">
                   <strong className="title"> {item.sender_id} </strong>
                   <p>
-                    {messagePreview(item.body)}
+                    {item.body}
                   </p>
                 </div>
               </li>
--- ui/src/Components/Messages.jsx
+++ ui/src/Components/Messages.jsx
@@ -4,17 +4,39 @@ import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import UserNavBar from "./UserNavBar";
 import Footer from "./Footer";
-import "../Styles/Massages.scss";
+import "../Styles/Messages.scss";
 import MessageItem from "./MessageItem";
 import ChatBlock from "./ChatBlock";
+
+let messagesEndpoint =
+  "https://k9g7kqmqtj.execute-api.us-east-1.amazonaws.com/dev/messages/";
+
+const axios = require("axios");
 
 const useStyles = makeStyles((theme) => ({}));
 
 export default function Messages() {
+  const [active, setActive] = React.useState(1);
   const classes = useStyles();
 
+  const [messageData, setMessageData] = React.useState(0);
+  var i = 1;
+
+  while (i == 1){
+    axios.get(messagesEndpoint + "33")
+    .then((res) => {
+      setMessageData(JSON.parse(res.data.body));
+      console.log(messageData);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+    i++;
+  }
+  
+
   // const messages = fetch(https://k9g7kqmqtj.execute-api.us-east-1.amazonaws.com/dev/messages/ )
-  let data = [
+  /* let data = [
     {
       message_id: 1,
       sender_id: 1,
@@ -31,7 +53,7 @@ export default function Messages() {
       body: "This is some next shit. This is ",
       m_timestamp: "2020-05-02"
     },
-  ];
+  ]; */
   return (
     <React.Fragment>
       <CssBaseline />
@@ -39,16 +61,16 @@ export default function Messages() {
       <main>
         <div className="massagesHolder">
           <div className="container">
-            <h2>🙌 Your messages, Serina.</h2>
+            <h2> 🙌Your messages, Serina. </h2>{" "}
             <div className="twoColumns">
-              <MessageItem data={data} />
+              <MessageItem messageData={messageData} />{" "}
               <div className="content">
                 <ChatBlock />
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+              </div>{" "}
+            </div>{" "}
+          </div>{" "}
+        </div>{" "}
+      </main>{" "}
       <Footer />
     </React.Fragment>
   );
